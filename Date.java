e6c5ce611de73e530f7db2890198f934ce17cd08
jgarca17 <jgarca17@estudiantes.unileon.es>
2016-04-07 12:16:15 +0200

    he subido el archivo con los ejercicios del switch finalizados

--- Date.java
+++ Date.java
@@ -2,9 +2,9 @@ package es.unileon.prg.date;
 
 
 	public class Date{
-		int day;
-		int month;
-		int year;
+		private int day;
+		private int month;
+		private int year;
 		public Date(int day, int month, int year){
 			this.day = day;
 			this.month = month;
@@ -28,7 +28,6 @@ package es.unileon.prg.date;
 		void setYear(int year){
 			this.year = year;
 			}
-		
 		boolean isSameDay(Date date){
 			return(this.Day == date.getDay()){
 		}
@@ -41,4 +40,105 @@ package es.unileon.prg.date;
 		boolean isSame(Date day, Date month, Date year){
 			return(this.year == date.getYear() && this.Month == date.getMonth() && this.Day == date.getDay()){
 		}
+	public String monthName(Date date){
+		String nameMonth = " ";
+		switch(date.getMonth()){
+		case 1: nameMonth = "Enero"; break;
+		case 2: nameMonth = "Febrero"; break;
+		case 3: nameMonth = "Marzo"; break;
+		case 4: nameMonth = "Abril"; break;
+		case 5: nameMonth = "Mayo"; break;
+		case 6: nameMonth = "Junio"; break;
+		case 7: nameMonth = "Julio"; break;
+		case 8: nameMonth = "Agosto"; break;
+		case 9: nameMonth = "Septiembre"; break;
+		case 10: nameMonth = "Octubre"; break;
+		case 11: nameMonth = "Noviembre"; break;
+		case 12: nameMonth = "Diciembre"; break;
+		default: nameMonth = "Mes no valido"; break;
+		}
+		
+		return nameMonth;
 	}
+	
+	public boolean correctDayOfMonth(Date date){
+		boolean correctDay = false;
+		switch(date.getMonth()){
+		case 1: 
+		case 3:
+		case 5:
+		case 7: 
+		case 8:
+		case 10:
+		case 12:
+			if(day.getDay()<=31){
+				correctDay = true;
+			}
+			break;
+		case 2: 
+			if(date.getYear()%4 == 0 && date.getYear()%100 != 0 && date.getDay()<=29){
+				correctDay = true;
+			}else if(date.getDay()<=28){
+				correctDay = true;
+			}
+			break;
+		case 4: 
+		case 6: 
+		case 9: 
+		case 11: 
+			if(date.getDay()<=30){
+				correctDay = true;
+			}
+			break; 
+		}
+		
+		return correctDay;
+	}
+	public String yearStation(Date date){
+		String station = " ";
+		switch(date.getMonth()){
+		case 1: 
+		case 2: 
+			station= "Invierno"; break;
+		case 3: 
+			if(date.getDay()>=21){
+				station = "Primavera";
+			}else{
+				station = "Invierno";
+			}
+			break;
+		case 4: 
+		case 5:
+			station = "Primavera"; break;
+		case 6: 
+			if(date.getDay()>=21){
+				station = "Verano";
+			}else{
+				station = "Primavera";
+			}
+			break;
+		case 7: 
+		case 8: 
+			station = "Verano"; break;
+		case 9: 
+			if(date.getDay()>=23){
+				station = "Otoño";
+			}else{
+				station = "Verano";
+			}
+			break;
+		case 10: 
+		case 11: 
+			station = "Otoño"; break;
+		case 12: 
+			if(date.getDay()>=21){
+				station = "Invierno";
+			}else{
+				station = "Otoño";
+			}
+			break;
+		}
+		
+		return station;
+	}
+	
